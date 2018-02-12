032ee07b12f399d3c8010502961ec730021bb557
A.C. Keizer <alex@keizer.tk>
2018-02-12 03:00:19 +0100

    merge all output of script to stdout

--- multimetric.sh
+++ multimetric.sh
@@ -23,9 +23,26 @@ mkdir tmp
 
 echo "running from" $1 "to" $2 "with" $((loops*workers)) "loops"
 for i in $(seq $workers); do
-	$EXE_PATH metric $1 $2 $loops > tmp/job_$i.csv &
+	$EXE_PATH metric $1 $2 $loops $H $W $RANDOM > tmp/job_$i.csv &
 	job[$i]=$!
 done
 
 wait ${job[@]}
-echo "done"
+
+cat $(for i in $(seq $workers); do echo tmp/job_$i.csv; done) | sort |
+sed -E -e '/^([0-9]+),([0-9]+)/{
+	   s/^([0-9]+(,[0-9]+){3}),([0-9]+)/\1,=\3/
+       $!{          # append the next line when not on the last line
+	     :loop
+		 N
+		 
+         s/^([0-9]+(,[0-9]+){3}),(=[0-9]+(\+[0-9]+)*)\s+\1,([0-9]+)/\1,\3\+\5/
+                    # now test for a successful substitution, otherwise
+                    #+  unpaired "a test" lines would be mis-handled
+         t loop  # branch_on_substitute (goto label :sub-yes)
+         :sub-not   # a label (not essential; here to self document)
+                    # if no substituion, print only the first line
+         P          # pattern_first_line_print
+         D          # pattern_ltrunc(line+nl)_top/cycle
+       }    
+     }'
