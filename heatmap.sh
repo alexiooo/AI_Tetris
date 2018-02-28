3984e12a1a17d5e13a03ccf100653bdf2fa8095a
alexiooo <alex@keizer.tk>
2018-02-28 14:43:09 +0100

    MOAR heatmap thnihys

--- heatmap.sh
+++ heatmap.sh
@@ -1,13 +1,16 @@
 EXE_PATH=./bin/Debug/AI_Tetris.exe
 
 WIDTH="4 8 12 16 20"
-HEIGHT="4 8 12 15"
+HEIGHT="8 12 15"
 
 MONTE=500
 TEST_SIZE=500
 
 for w in $WIDTH; do
 	for h in $HEIGHT; do
-		$EXE_PATH benchcarlo $h $w $MONTE $TEST_SIZE $RANDOM | tail +2
+		$EXE_PATH smart $h $w -1 5 15 -20 -10 $TEST_SIZE $RANDOM
+		echo ""
 	done
 done
+
+#$EXE_PATH benchcarlo $h $w $MONTE $TEST_SIZE $RANDOM
