f5407e87205901d1b2e1a7e4f78d743f1ecde4b9
alexiooo <alex@keizer.tk>
2018-03-01 06:24:34 +0100

    ...

--- heatmap.sh
+++ heatmap.sh
@@ -4,11 +4,11 @@ WIDTH="4 8 12 16 20"
 HEIGHT="8 12 15"
 
 MONTE=500
-TEST_SIZE=500
+TEST_SIZE=$1
 
 for w in $WIDTH; do
 	for h in $HEIGHT; do
-		$EXE_PATH smart $h $w -1 5 15 -20 -10 $TEST_SIZE $RANDOM
+		$EXE_PATH smart $h $w -10 50 150 -200 -10 $TEST_SIZE $RANDOM
 		echo ""
 	done
 done
